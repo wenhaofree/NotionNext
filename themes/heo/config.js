8e86ca5170721078850f0efdc07b92865d4b8eca
fwh-mac-mini <fuwenhao594@163.com>
2024-10-09 10:00:17 +0800

    fix: URL

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -118,7 +118,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://blog.fuwenhao.club/groups',
+  HEO_SOCIAL_CARD_URL: 'https://blog.fuwenhao.club/article/432c2af4-a353-4fb0-95c0-3dfb824a61cb',
   //HEO_SOCIAL_CARD_URL: 'https://blog.fuwenhao.club/article/groups',
 
   // *****  以下配置无效，只是预留开发 ****
