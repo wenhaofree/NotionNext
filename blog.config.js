a50b11f444b4409f9ff5eeab90d22c9bf3b77f73
FwhMacMini <fuwenhao594@163.com>
2024-12-24 15:15:42 +0800

    fix: link

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '您的数字宝藏收藏夹📂', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.wenhaofree.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://yzzzzy.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
