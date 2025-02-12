104d8a37664dab1f61a74c119084f94a7cd547fb
FwhMacMini <fuwenhao594@163.com>
2025-02-12 14:05:26 +0800

    fix: 交流群URL

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://blog.fuwenhao.club/article/432c2af4-a353-4fb0-95c0-3dfb824a61cb',
+  HEO_SOCIAL_CARD_URL: 'https://blog.wenhaofree.com/article/432c2af4-a353-4fb0-95c0-3dfb824a61cb',
   //HEO_SOCIAL_CARD_URL: 'https://blog.fuwenhao.club/article/groups',
 
   // 底部统计面板文案
