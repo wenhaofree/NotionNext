1b71701dd5549ebea02eec469f835000172d6d42
FwhMacMini <fuwenhao594@163.com>
2025-04-21 11:16:30 +0800

    fix: umami

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import BLOG from '@/blog.config'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+// import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -49,12 +49,15 @@ class MyDocument extends Document {
             `}
           </script>
           
+          
           {/* Umami追踪代码 */}
-          <Script
+          <script defer src="https://umami.wenhaofree.com/script.js" data-website-id="4d7f02e3-4a29-45f1-99e6-aa562d665f3c"></script>
+
+          {/* <Script
             src="https://umami.wenhaofree.com/script.js"
             data-website-id="4d7f02e3-4a29-45f1-99e6-aa562d665f3c"
             defer
-          />
+          /> */}
         </Head>
 
         <body>
